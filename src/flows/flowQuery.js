1d20a0f5fd8a0f933d02f81194917026c8dd71a9
Isaac <carlin_isaac@hotmail.com>
2023-09-29 17:28:21 -0400

    add flow ubicacion

--- src/flows/flowQuery.js
+++ src/flows/flowQuery.js
@@ -1,18 +1,38 @@
 const { addKeyword } = require("@bot-whatsapp/bot");
 
-const flowConsulta = addKeyword(["pregunta", 'consulta', 'tenes']).addAnswer(
-    "Esperame un momento voy a verificar",
-    // TODO: HACER CONSULTA A BASE DE DATOS
-    null,
-    async (ctx) => {
-      console.log(ctx);
-      const numeroDeWhatsapp = ctx.from;
-      const mensajeRecibido = ctx.body;
-    }
-  );
+const flowUbicacion = addKeyword(["ubicacion", "donde estan","ubicados", "local"]).addAnswer(
+  "estamos ubicados en tu corazon",
+  null,
+  async (ctx) => {
+    console.log(ctx);
+    const numeroDeWhatsapp = ctx.from;
+    const mensajeRecibido = ctx.body;
+  }
+);
 
+const flowProductos = addKeyword(["tienen", "tenes"]).addAnswer(
+  "Esperame un momento voy a verificar",
+  null,
+  async (ctx) => {
+    console.log(ctx);
+    const numeroDeWhatsapp = ctx.from;
+    const mensajeRecibido = ctx.body;
+  }
+);
+
+const flowConsulta = addKeyword(["pregunta", "consulta"]).addAnswer(
+  "Si, cual es tu consulta",
+  // TODO: HACER CONSULTA A BASE DE DATOS
+  null,
+  async (ctx) => {
+    console.log(ctx);
+    const numeroDeWhatsapp = ctx.from;
+    const mensajeRecibido = ctx.body;
+  },
+  [flowProductos, flowUbicacion]
+);
 
 module.exports = {
-  flowConsulta
+  flowConsulta,
+  flowUbicacion
 };
-  
