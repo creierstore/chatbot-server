9c2dc65ea0376839dbb70d30c4ea868312bd9575
pedro <pedro.jcaceresl@gmail.com>
2023-10-07 18:08:40 -0300

    Add: flujo Envio y Principal     Definicion de flujos

--- src/flows/flujoEnvio.js
+++ src/flows/flujoEnvio.js
@@ -1,3 +1,22 @@
 // Despedida
 // Envuesta
 // Respuesta
+
+const { addKeyword } = require("@bot-whatsapp/bot");
+const { flujoDespedida } = require("./flujoDespedida");
+
+
+
+const keywords = [
+    "envio"
+];
+const response = [
+	"Si, tenemos servicio de delivery con costo adicional, 20 mil Gs",
+];
+
+
+const flujoEnvio = addKeyword(keywords).addAnswer(response, null, null, [flujoDespedida]);
+
+module.exports = {
+	flujoEnvio,
+};
--- src/flows/flujoPrincipal.js
+++ src/flows/flujoPrincipal.js
@@ -1,4 +1,5 @@
 const { addKeyword } = require("@bot-whatsapp/bot");
+const { flujoEnvio } = require("./flujoEnvio");
 
 // Flujo Consulta
 // Flujo Pedido
@@ -8,20 +9,24 @@ const { addKeyword } = require("@bot-whatsapp/bot");
 
 // Importar todo
 
+const keywords = ["hola", "buenas"];
+const response = ["Bienvenidos a Versol Informática, en que te podemos ayudar?"];
+const flujos = [flujoEnvio]
 
+// const flowBienvenida = addKeyword(['hola', 'buenas']).addAnswer(
+//     "Bienvenidos a X, en que te puedo ayudar?",
+//     null,
+//     async (ctx) => {
+//       console.log(ctx);
+//       const numeroDeWhatsapp = ctx.from;
+//       const mensajeRecibido = ctx.body;
+//     }
+//   );
 
-const flowBienvenida = addKeyword(['hola', 'buenas']).addAnswer(
-    "Bienvenidos a X, en que te puedo ayudar?",
-    null,
-    async (ctx) => {
-      console.log(ctx);
-      const numeroDeWhatsapp = ctx.from;
-      const mensajeRecibido = ctx.body;
-    }
+const flujoPrincipal = addKeyword(keywords).addAnswer(response, null, null, flujos
   );
 
-
 module.exports = {
-flowBienvenida
+// flowBienvenida
 };
   
