00728711ceb03ce092085a6835ef664ce6a9ca9c
Isaac <carlin_isaac@hotmail.com>
2023-10-18 16:00:14 -0300

    fix: await on the main function

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const {app, main} = require("./app.js");
 require("dotenv").config();
 
-const PORT = process.env.PORT ?? 8200;
+const PORT = process.env.PORT || 8200;
 
 async function start() {
   try {
     console.log("Connection has been established successfully.");
     app.listen(PORT);
-    main();
+    await main();
     console.log("Server is listening on port", PORT);
   } catch (error) {
     console.error("Unable to connect to the database:", error);
