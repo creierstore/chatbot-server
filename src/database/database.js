e01d488654ea19fd74287ac6de17a9a0eb40bce7
Isaac <carlin_isaac@hotmail.com>
2023-10-18 09:04:40 -0300

    CHORE: clean project

--- src/database/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize("chatbot", "postgres", "admin", {
-  host: "localhost",
-  dialect: "postgres",
-  port: '5434'
-});
-
-module.exports = sequelize;
