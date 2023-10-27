569ea18db8ff64b2617b01004dcab5a1875cc031
Isaac <carlin_isaac@hotmail.com>
2023-10-27 09:58:15 -0300

    (fix): ya se leen las variables de entorno
        Se ha establecido como directorio de trabajo, el directorio raiz
        lo que permite la lectura de variables de entorno.

--- src/dialogflow/dialogflow.js
+++ src/dialogflow/dialogflow.js
@@ -1,53 +1,37 @@
+const dialogflow = require("dialogflow");
+const structjson = require("structjson");
+const path = require("path");
 
+process.chdir(path.resolve(__dirname, "../../"));
+require("dotenv").config();
 
-
-const dialogflow = require('dialogflow');
-const structjson = require('structjson');
-require('dotenv').config();
-
-
-
-
-const { addKeyword, addAnswer } = require("@bot-whatsapp/bot");
-
-
-// console.log(process.env);
-
-
-// Replace these values with your own
-const projectId = "vendedorbot-bgkb";
 const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
-
-
+const projectId = "vendedorbot-bgkb";
 const CONFIGURATION = {
-	credentials: {
-		private_key: CREDENTIALS["private_key"],
-		client_email: CREDENTIALS["client_email"],
-	},
+  credentials: {
+    private_key: CREDENTIALS["private_key"],
+    client_email: CREDENTIALS["client_email"],
+  },
 };
 
 const sessionClient = new dialogflow.SessionsClient(CONFIGURATION);
 
-// console.log(CONFIGURATION);
-
 async function detectIntent(text, sessionId) {
   // The path to your Dialogflow agent's project ID
   const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-
   // The text query from the user
   const request = {
     session: sessionPath,
     queryInput: {
       text: {
         text: text,
-        languageCode: 'es', // Replace with the desired language code
+        languageCode: "es",
       },
     },
   };
 
   try {
     const responses = await sessionClient.detectIntent(request);
-    // console.log(JSON.stringify(responses, 0, 2));
     const result = responses[0].queryResult;
     const intent = result.intent.displayName;
     const parameters = structjson.structProtoToJson(result.parameters);
@@ -57,23 +41,9 @@ async function detectIntent(text, sessionId) {
       parameters,
     };
   } catch (error) {
-    console.error('Error detecting intent:', error);
+    console.error("Error detecting intent:", error);
     throw error;
   }
 }
 
-// Example usage
-const userInput = 'alo!';
-const sessionId = 'unique-session-id'; // You can generate a unique session ID
-
-// detectIntent(userInput, sessionId)
-//   .then((response) => {
-//     console.log('Detected Intent:', response.intent, {response});
-//     console.log('Parameters:', response.parameters);
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error);
-//   });
-
-
-  module.exports = {detectIntent}
+module.exports = { detectIntent };
